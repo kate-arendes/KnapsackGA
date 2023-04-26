4739afd6c3c4ae03071e10091d5ea15482c0395e
kma9q7 <93099088+kma9q7@users.noreply.github.com>
2023-04-26 12:03:55 -0500

    Works properly for one problem instance, need a way to do all three different problem instances. Gonna try to implement that, pls pray

--- Population.py
+++ Population.py
@@ -1,12 +1,209 @@
 from Chromosome import Chromosome
+from Chromosome import CHROM_LENGTH
 import random
-import math
+
 
 class Population:
-    def __init__(self, size, gen):
+    def __init__(self, size):
         self.size = size
+        self.gen = 0
 
         self.chromosomes = []
+        self.fitnesses = []
+        self.dist = []
+        self.sampled = []
+        self.crossed = []
 
         for i in range(self.size):
             self.chromosomes.append(Chromosome())
+
+    def print_pop(self):
+        for i in range(self.size):
+            self.chromosomes[i].print_chrom()
+            print(self.chromosomes[i].get_value())
+
+    def get_best(self):
+        f_best = 0.0
+        best_chrom = Chromosome()
+        best_chrom.bitstring = ""
+
+        for i in range(CHROM_LENGTH):
+            best_chrom.bitstring += "0"
+
+        for i in range(self.size):
+            if self.chromosomes[i].get_value() > f_best and self.chromosomes[i].is_valid():
+                f_best = self.chromosomes[i].get_value()
+                best_chrom = self.chromosomes[i]
+        return best_chrom
+
+    def penalty_static(self):
+        for i in range(self.size):
+            if self.chromosomes[i].is_valid():
+                self.fitnesses.append(self.chromosomes[i].get_value())
+            else:
+                new_fitness = self.chromosomes[i].get_value() - (self.chromosomes[i].get_diff())
+                if new_fitness <= 0:
+                    new_fitness = 0.001
+                self.fitnesses.append(new_fitness)
+
+    def penalty_dynamic(self):
+        for i in range(self.size):
+            if self.chromosomes[i].is_valid():
+                self.fitnesses.append(self.chromosomes[i].get_value())
+            else:
+                new_fitness = self.chromosomes[i].get_value() - (1 + 0.01 * self.gen) * (self.chromosomes[i].get_diff())
+                if new_fitness <= 0:
+                    new_fitness = 0.001
+                self.fitnesses.append(new_fitness)
+
+    def prop_selection(self):
+        fit_sum = 0.0
+        for i in range(self.size):
+            fit_sum += self.fitnesses[i]
+
+        prob_sum = 0.0
+        for i in range(self.size):
+            self.dist.append(prob_sum + (self.fitnesses[i] / fit_sum))
+            prob_sum += self.fitnesses[i] / fit_sum
+
+    def stoch_uni_sampling(self):
+        point = random.uniform(0, 1)
+        interval = 1.0 / self.size
+        for i in range(self.size):
+            current_point = (i * interval) + point
+            if current_point > 1:
+                current_point -= 1
+            for j in range(self.size):
+                if current_point < self.dist[j]:
+                    new_chrom = Chromosome()
+                    new_chrom.chrom_copy(self.chromosomes[j])
+                    self.sampled.append(new_chrom)
+                    break
+        self.chromosomes.clear()
+        self.fitnesses.clear()
+        self.dist.clear()
+
+    def one_point_cross(self, prob_c):
+        # Picks pairs of chromosomes and performs crossover
+
+        iterations = int(self.size / 2)
+
+        # When two chromosomes are picked, they are removed from the selected list
+
+        for i in range(iterations):
+            index1 = random.randint(0, (len(self.sampled) - 1))
+            chrom1 = self.sampled[index1]
+            del self.sampled[index1]
+
+            index2 = random.randint(0, (len(self.sampled) - 1))
+            chrom2 = self.sampled[index2]
+            del self.sampled[index2]
+
+            # Calculates whether crossover occurs and if so, performs the crossover using gene_swap()
+
+            if random.uniform(0, 1) < prob_c:
+                self.gene_swap(chrom1, chrom2)
+
+            # Adds the crossed chromosomes back into the population's chromosome list
+
+            self.crossed.append(chrom1)
+            self.crossed.append(chrom2)
+
+        # If there's an odd population size, the last chromosome is added to the list of chromosomes to be mutated
+
+        if self.sampled:
+            self.crossed.append(self.sampled[0])
+
+    # gene_swap() performs a gene swap operation for two chromosomes starting at a randomized point
+
+    def gene_swap(self, first_chrom, second_chrom):
+
+        # Generate crossover point
+
+        point = random.randint(1, len(first_chrom.bitstring) - 1)
+
+        # Create resulting child bit strings
+
+        child1 = (first_chrom.bitstring[0:point] + second_chrom.bitstring[point:])
+        child2 = (second_chrom.bitstring[0:point] + first_chrom.bitstring[point:])
+
+        # Replace parental bit strings with children's bit string
+
+        first_chrom.bitstring = child1
+        second_chrom.bitstring = child2
+
+    def uni_cross(self):
+
+        iterations = int(self.size / 2)
+
+        for i in range(iterations):
+            index1 = random.randint(0, (len(self.sampled) - 1))
+            chrom1 = self.sampled[index1]
+            del self.sampled[index1]
+
+            index2 = random.randint(0, (len(self.sampled) - 1))
+            chrom2 = self.sampled[index2]
+            del self.sampled[index2]
+
+            self.perform_uni_cross(chrom1, chrom2)
+
+            self.crossed.append(chrom1)
+            self.crossed.append(chrom2)
+
+        if self.sampled:
+            self.crossed.append(self.sampled[0])
+
+    def perform_uni_cross(self, first_chrom, second_chrom):
+
+        child1 = ""
+        child2 = ""
+
+        for i in range(len(first_chrom.bitstring)):
+            prob = random.randint(0, 1)
+            if prob == 0:
+                child1 += first_chrom.bitstring[i]
+                child2 += second_chrom.bitstring[i]
+            else:
+                child1 += second_chrom.bitstring[i]
+                child2 += first_chrom.bitstring[i]
+
+        first_chrom.bitstring = child1
+        second_chrom.bitstring = child2
+
+
+    def multi_parent_cross(self):
+
+        for i in range(self.size):
+            parents = []
+            for j in range(3):
+                index = random.randint(0, (len(self.sampled) - 1))
+                parents.append(self.sampled[index])
+            self.crossed.append(self.maj_vote(parents))
+        self.sampled.clear()
+
+    def maj_vote(self, chroms):
+
+        child = Chromosome()
+        child.bitstring = ""
+
+        for i in range(len(chroms[1].bitstring)):
+            zeros = 0
+            ones = 0
+            for j in range(3):
+                if chroms[j].bitstring[i] == "0":
+                    zeros += 1
+                else:
+                    ones += 1
+            if zeros > ones:
+                child.bitstring += "0"
+            else:
+                child.bitstring += "1"
+
+        return child
+
+
+    def mutation(self, prob_m):
+        for i in range(len(self.crossed)):
+            self.crossed[i].mutate(prob_m)
+            self.chromosomes.append(self.crossed[i])
+        self.crossed.clear()
--- main.py
+++ main.py
@@ -1,9 +1,96 @@
 from Chromosome import Chromosome
+from Chromosome import CHROM_LENGTH
+from Population import Population
+import random
+import statistics
+
+RUNS = 30
+
+
+def evolve(members, generations, cross_prob, mut_prob, pen_type, cross_type):
+    pop = Population(members)
+
+    best_solution = pop.get_best()
+    best_fit = pop.get_best().get_value()
+
+    for i in range(generations):
+        if pen_type == 1:
+            pop.penalty_static()
+        else:
+            pop.penalty_dynamic()
+
+        pop.prop_selection()
+        pop.stoch_uni_sampling()
+
+        if cross_type == 1:
+            pop.one_point_cross(cross_prob)
+        elif cross_type == 2:
+            pop.uni_cross()
+        else:
+            pop.multi_parent_cross()
+
+        pop.mutation(mut_prob)
+
+        if pop.get_best().get_value() > best_fit:
+            best_solution = pop.get_best()
+            best_fit = pop.get_best().get_value()
+
+    return best_solution
+
 
 if __name__ == '__main__':
-    chrom = Chromosome()
-    print(chrom.bitstring)
-    print(chrom.get_weight())
-    print(chrom.get_value())
-    print(chrom.get_diff())
-    print(chrom.is_valid())
+
+    # Prompts the user for penalty type and crossover type
+
+    print("\n1. Static Penalty\n2. Dynamic Penalty\n\nPlease select a type of penalty:\n")
+    p_type = int(input("Penalty type: "))
+
+    print("\n1. One-Point\n2. 0.5 Uniform\n3. Multi-Parent (Majority Voting)\n\nPlease select a type of penalty:\n")
+    c_type = int(input("Crossover type: "))
+
+    print("\nRunning the genetic algorithm . . .\n")
+
+    # Collects the best solution from each run and its fitness value
+
+    best_solutions = []
+    best_f_vals = []
+
+    # Tracks the best solution over all independent runs
+
+    overall_f_best = 0
+    overall_best_chrom = Chromosome()
+    overall_best_chrom.bitstring = ""
+    for i in range(CHROM_LENGTH):
+        overall_best_chrom.bitstring += "0"
+
+    # Conducts independent runs
+
+    for j in range(RUNS):
+
+        # Seeds random so that results are reproducible
+
+        random.seed(j)
+
+        # Adds the best solution to the running list
+
+        best_solutions.append(evolve(30, 50, 0.7, 0.05, p_type, c_type))
+        best_f_vals.append(best_solutions[j].get_value())
+
+        # Determines if the best solution is the overall best
+
+        if best_solutions[j].get_value() > overall_f_best:
+            overall_best_chrom = best_solutions[j]
+            overall_f_best = best_solutions[j].get_value()
+
+    # Prints all the results from the independent runs
+
+    print("\n============ Results from Thirty Independent Runs ============\n")
+    print("Overall Best Fitness Value: " + str('{:.20f}'.format(overall_f_best)))
+    print("Mean of Best Fitness Values: " + str('{:.20f}'.format(statistics.mean(best_f_vals))))
+    print("Stdev of Best Fitness Values: " + str('{:.20f}'.format(statistics.stdev(best_f_vals))))
+
+    print("\n\n==================== Best Overall Solution ====================\n")
+    print("Bit-String Chromosome:\n" + overall_best_chrom.bitstring)
+    print("\nWeight of Items:\n" + str('{:.20f}'.format(overall_best_chrom.get_weight())))
+    print("\nValue of Items:\n" + str('{:.20f}'.format(overall_f_best)))
+    print()
